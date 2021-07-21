c488c9f1e508726157453f3696bd4c3dabe5e965
Danny Morris <usc00@hotmail.com>
2021-07-21 19:38:38 -0400

    Update hourly.py
    Update

--- hourly.py
+++ hourly.py
@@ -8,4 +8,5 @@ def get_hourly_weather(station, days_history = 30, end = datetime.now()):
     df['time_date'] = df['time'].dt.date
     df['time_date'] = pd.to_datetime(df['time_date'])
     df['time_date'] = df['time_date'].dt.tz_localize(tz='EST')
+    df['time'] = df['time'].dt.tz_localize(tz='EST')
     return(df)
